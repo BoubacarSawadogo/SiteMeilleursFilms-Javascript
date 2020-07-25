9da97eb9dfe3b4acf414cc7fbc5e3dfc994b57f6
Boubacar Sawadogo <bsawadog@isimtl.local>
2020-07-25 18:21:06 -0400

    display details ok

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ window.onload = function () {
   // Creation d'un film
   function createFilm(number) {
     let someFilm = filmData[number];
+    someFilm.id = number;
 
     // div qui contient un film
     let filmConteneur = document.createElement("div");
@@ -12,6 +13,7 @@ window.onload = function () {
     let image = document.createElement("img");
     image.src = someFilm.image;
     image.alt = someFilm.title;
+    image.id = someFilm.id;
 
     let titre = document.createElement("h3");
     titre.innerHTML = someFilm.title;
@@ -23,8 +25,20 @@ window.onload = function () {
     let conteneurGeneralFilm = document.getElementById("films");
     conteneurGeneralFilm.appendChild(filmConteneur);
 
+    filmConteneur.addEventListener("click", function (event) {
+      console.log(filmData[event.target.id]);
+      try {
+        document
+          .getElementById("selection1")
+          .appendChild(filmData[event.target.id]);
+      } catch (error) {}
+      document.getElementById("details").innerHTML =
+        filmData[event.target.id].text;
+    });
+
     //Ajout evenement onClick
-  }
+  } //****FIN***/
+
   // Affichage de tout les films
   let i = 0;
   for (let i = 0; i < filmData.length; i++) {
@@ -34,6 +48,8 @@ window.onload = function () {
   // Filtrage des films par mot cle
   let inputFilter = document.getElementById("filter");
   inputFilter.addEventListener("keyup", rechercher);
+  let afficherDetails = document.getElementById("AfficherDetails");
+  afficherDetails.addEventListener("input", afficherDeatailsFilm);
 
   function rechercher(event) {
     let inputValue = event.target.value;
@@ -51,7 +67,16 @@ window.onload = function () {
     }
   }
 
-  // Autre moyen d'afficher dynamique tous les films
+  function afficherDeatailsFilm() {
+    console.log(afficherDetails.checked);
+    if (!afficherDetails.checked) {
+      document.getElementById("details").style.display = "none";
+    } else {
+      document.getElementById("details").style.display = "inline-block";
+    }
+  }
+
+  // Autre moyen d'afficher dynamiqueMENT tous les films
   /* function addFilms() {
     let films = filmData.slice();
     let filmmm = "";
